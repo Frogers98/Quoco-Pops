00d9afbf919923f12232a7c1f86a243cc7f6cd01
Finnian Rogers <finnianrogers@gmail.com>
2022-01-04 17:22:27 +0000

    changed application.conf in borrow to use correct MySerializable

--- borrow/src/main/java/borrow/LoanService.java
+++ borrow/src/main/java/borrow/LoanService.java
@@ -46,6 +46,7 @@ public class LoanService extends AbstractActor {
 
         // Register Actor with the Broker
         ActorSelection selection = loanSystem.actorSelection("akka.tcp://default@127.0.0.1:2551/user/broker");
+        System.out.println(selection.toString());
         selection.tell("registerLoan", ref);
 
         // Open a database connection
